18ac788d749fa663d926c644cd8dbcadff1e6e7c
Rishabh Bose <Rishabhbose3@gmail.com>
2022-10-19 15:16:00 +0530

    bugfix

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 const port = 3000;
 
+// GET Services FROM API
 app.post("/", (req, res) => {
   const { key, action } = req.body;
   const services = {
@@ -33,6 +34,34 @@ app.post("/", (req, res) => {
       console.log(error);
     });
 });
+
+// POST NEW ORDER TO API
+app.post("/neworder", (req, res) => {
+  const { key, action, service, link, quantity } = req.body;
+  const newOrder = {
+    key,
+    action,
+    service,
+    link,
+    quantity,
+  };
+
+  axios
+    .post("https://indianprovider.com/api/v2", newOrder)
+    .then(function (response) {
+      console.log("res", response.data);
+      res.status(201).json(response.data);
+    })
+    .catch(function (error) {
+      res.status(400).str(error);
+      console.log(error);
+    });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
 app.use("/user", userRoutes);
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening on port ${port}!`)
